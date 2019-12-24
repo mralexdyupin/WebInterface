49c1a91a1fe0803ab60e82675e825ac92cdb6938
mralexdyupin <mr.alexdyupin@gmail.com>
2019-12-24 20:03:05 +0400

    upgrade

--- src/test/java/CallBackTest.java
+++ src/test/java/CallBackTest.java
@@ -1,11 +1,11 @@
-import com.sun.org.apache.xpath.internal.operations.Equals;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,50 +28,54 @@ public class CallBackTest {
         driver = null;
     }
 
-    @Test
-    void shouldWork() throws InterruptedException {
-        driver.get(" http://localhost:9999");
+    /*Рабочие тесты позитивный сценарий, добавить @DisplayName*/
 
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Александр");
-        elements.get(1).sendKeys("+79127468103");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-        Thread.sleep(5000);
+    @ParameterizedTest
+    @CsvFileSource(resources = "/NamePositiveData.csv", numLinesToSkip = 1)
+    void TestWebFormV2(String name) {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys(name);
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String result = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", result.trim());
+//        Thread.sleep(5000);
+    }
 
-       /* WebElement form = driver.findElement(By.);
-        form.findElement(By.cssSelector("[data-test-id=name]input")).sendKeys("Василий");
-        form.findElement(By.cssSelector("[data-test-id=phone]input")).sendKeys("+79124768103");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
-        String text=driver.findElement(By.className("order-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
-        Thread.sleep(5000);*/
+    @ParameterizedTest
+    @CsvFileSource(resources = "/NameNegativeData.csv", numLinesToSkip = 1)
+    void TestWebFormV3(String name) {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys(name);
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String result = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", result.trim());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/PhoneNegativeData.csv", numLinesToSkip = 1)
+    void TestWebFormPhone(String phone) {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("Тест тест");
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys(phone);
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector(".button")).click();
+        String result = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", result.trim());
     }
 
     @Test
-    void shouldNameError() {
-        driver.get(" http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("123test");
-        elements.get(1).sendKeys("+79127468103");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
-        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    void TestWebFormV4() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("Тест Тест");
+        driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79999999999");
+        driver.findElement(By.cssSelector(".button")).click();
+        String result = driver.findElement(By.cssSelector("[data-test-id=agreement]")).getCssValue("color");
+        assertEquals("rgba(255, 92, 92, 1)", result);
+//        Thread.sleep(5000);
     }
-
-    @Test
-    void shouldPhoneError() {
-        driver.get(" http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Тест Тестовый");
-        elements.get(1).sendKeys("+791274688888");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("input__sub")).getText();
-        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
-    }
+    /*Тесты на валидацию, т.е. с пустыми полями*/
 }
